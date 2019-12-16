f65f767b4864273594d32d22e39ab9c3545d2aae
roberto <a.robertofischer@gmail.com>
2019-12-16 22:09:40 +0100

    improvemtn

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,22 +1,21 @@
+const VERSES: [&'static str; 3] = [
+    "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n",
+    "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n",
+    "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+];
+
 pub fn verse(n: u32) -> String {
-    if n == 0 {
-        String::from(
-            "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n",
-        )
-    } else if n == 1 {
-        String::from(
-            "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n",
-        )
-    } else if n == 2 {
-        String::from(
-            "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n",
-        )
-    } else {
-        format!(
-            "{0} bottles of beer on the wall, {0} bottles of beer.\nTake one down and pass it around, {1} bottles of beer on the wall.\n",
+    match n {
+        0 => VERSES[0].to_string(),
+        1 => VERSES[1].to_string(),
+        2 => VERSES[2].to_string(),
+        _ => format!(
+            "{0} bottles of beer on the wall, {0} bottles of beer.\n\
+             Take one down and pass it around, {1} bottles of beer \
+             on the wall.\n",
             n,
             n - 1
-        )
+        ),
     }
 }
 
