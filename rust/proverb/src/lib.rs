208a678cd52a6af85a47c5e1fc76210bf615aeaf
Roberto <a.robertofischer@gmail.com>
2019-12-17 15:29:50 +0100

    ADD proverb
    improvements

---
+++ rust/proverb/src/lib.rs
@@ -0,0 +1,13 @@
+use std::iter::once;
+
+pub fn build_proverb(list: &[&str]) -> String {
+    match list.first() {
+        None => String::new(),
+        Some(head) => list
+            .windows(2)
+            .map(|x| format!("For want of a {} the {} was lost.", x[0], x[1]))
+            .chain(once(format!("And all for the want of a {0}.", head)))
+            .collect::<Vec<String>>()
+            .join("\n"),
+    }
+}
