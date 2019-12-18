9657cf6ff2b3a737320423cbf37c0e452c8f66cb
Roberto <a.robertofischer@gmail.com>
2019-12-18 16:39:34 +0100

    Improve matching brackets

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,41 +1,27 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
     let mut stack = Vec::new();
-    let mut index = 0;
 
-    loop {
-        if string[index..].starts_with('{') {
-            stack.push('{');
-            index += 1;
-        } else if string[index..].starts_with('[') {
-            stack.push('[');
-            index += 1;
-        } else if string[index..].starts_with('(') {
-            stack.push('(');
-            index += 1;
-        } else if string[index..].starts_with('}') {
-            match stack.pop() {
-                Some('{') => index += 1,
-                Some(_unexpected) => break false,
-                None => break false,
+    for c in string.chars() {
+        match c {
+            '(' | '{' | '[' => stack.push(c),
+            ')' => {
+                if stack.pop() != Some('(') {
+                    return false;
+                }
             }
-        } else if string[index..].starts_with(']') {
-            match stack.pop() {
-                Some('[') => index += 1,
-                Some(_unexpected) => break false,
-                None => break false,
+            ']' => {
+                if stack.pop() != Some('[') {
+                    return false;
+                }
             }
-        } else if string[index..].starts_with(')') {
-            match stack.pop() {
-                Some(')') => index += 1,
-                Some(_unexpected) => break false,
-                None => break false,
+            '}' => {
+                if stack.pop() != Some('{') {
+                    return false;
+                }
             }
-        } else if index == string.len() && stack.is_empty() {
-            break true;
-        } else if index < string.len() {
-            index += 1;
-        } else {
-            break false;
+            _ => (),
         }
     }
+
+    stack.is_empty()
 }
