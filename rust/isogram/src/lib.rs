edfa1feb02c99b72927d81f0660e9796581c974d
roberto <a.robertofischer@gmail.com>
2022-03-23 02:01:05 +0100

    FUN

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -1,3 +1,16 @@
+use std::collections::HashSet;
+
 pub fn check(candidate: &str) -> bool {
-    unimplemented!("Is {} an isogram?", candidate);
+    candidate
+        .chars()
+        .filter(|c| c.is_alphabetic())
+        .map(|c| c.to_ascii_lowercase())
+        .try_fold(HashSet::new(), |mut acc, c| match acc.get(&c) {
+            None => {
+                acc.insert(c);
+                Some(acc)
+            }
+            Some(_) => None,
+        })
+        .map_or_else(|| false, |_| true)
 }
